92290838b8a4c110ae6e1bd28a94e32bb0ba7a07
JaviJinble <127382154+JaviJinble@users.noreply.github.com>
2023-03-16 19:00:46 +0900

    Add files via upload

---
+++ Chapter06/src/sec02/ex01/login2Servlet.java
@@ -0,0 +1,57 @@
+package sec02.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class login2Servlet
+ */
+public class login2Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init 메소드 호출");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		System.out.println("destroy 메소드 호출");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+	
+		String user_id = request.getParameter("user_id");
+		String user_pw = request.getParameter("user_pw");
+		
+		String data = "<html>";
+		data += "<body>";
+		data += "아이디 : " + user_id;
+		data += "<br>";
+		data += "비밀번호 : " + user_pw;	
+		data += "</body>";
+		data += "</html>";
+		
+		out.println(data);
+	}
+
+}
